6ee325f947f0172bd432029164a6cb933c85e754
Rachid Telfort <rachidt1994@gmail.com>
2019-02-01 18:31:17 -0500

    Added additional files via upload

---
+++ Scripts/DayTimeController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class DayTimeController:MonoBehaviour
+{
+    bool dayTimeActive = true;
+
+    static float currentTime=0.0f;
+
+    //The current stop time is 120 second or 2 minutes.
+    //Once two minutes passes and the player has not found the desired animal, the game is over/restarts.
+    //We can change this later if need be.
+    const float stopTime=120.0f;
+
+    [SerializeField]
+    static Light dayTimeLight;
+
+    private void Awake()
+    {
+        dayTimeLight = GetComponent<Light>();
+
+        dayTimeActive = true;
+        ResetDayTime();
+    }
+
+    //This will update the daytime from light to dark.
+    void Update()
+    {
+        UpdateDayTime();
+    }
+
+    //This is the main function that will update the daytime.
+    //The daytime is determined by the intensity of the player's directional light.
+    void UpdateDayTime()
+    {
+        if (dayTimeActive && currentTime < 120.0f)
+        {
+            currentTime += Time.deltaTime;
+
+            dayTimeLight.intensity = 1.0f - currentTime / stopTime;
+        }
+    }
+
+    public static void ResetDayTime()
+    {
+        currentTime=0.0f;
+        dayTimeLight.intensity=1.0f;
+    }
+
+    public static float getCurrentTime()
+    {
+        return currentTime;
+    }
+}
